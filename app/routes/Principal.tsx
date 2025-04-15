c26875baf0429edaaa64024f19d2ad753a60102f
LauraVert23 <laura.ribeiro@vert-capital.com>
2025-04-15 12:51:19 -0300

    Banner pagina principal

--- app/routes/Principal.tsx
+++ app/routes/Principal.tsx
@@ -1,7 +1,8 @@
-import { Outlet, useLoaderData } from "react-router";
+import { useLoaderData } from "react-router";
 import type { FilmeProps } from "~/interfaces/filmeProps";
 import { CarouselDemo } from "~/components/Carrossel";
 import Menu from "~/components/Menu";
+import LogoCinza from "../images/LogoCinza.png";
 import type { Route } from "./+types/home";
 
 export function meta({}: Route.MetaArgs) {
@@ -20,8 +21,29 @@ export default function PaginaPrincipal() {
   const filmes = useLoaderData() as FilmeProps[];
   return (
     <div>
-      <Menu />
-      <div></div>
+      <div className="absolute w-full z-10">
+        <Menu />
+      </div>
+      <div>
+        <div className=" ">
+          <div className="grid grid-cols-3 gap-2 w-[100%] h-[400px] relative  ">
+            <div className="absolute inset-0 flex justify-center items-center ">
+              <img
+                className="w-[110px] h-auto opacity-75"
+                src={LogoCinza}
+              ></img>
+            </div>
+            {filmes.slice(0, 9).map((filme: FilmeProps) => (
+              <div key={filme.id} className="flex justify-center ">
+                <img
+                  className="w-full h-auto object-cover rounded-md "
+                  src={`https://image.tmdb.org/t/p/original${filme.backdrop_path}`}
+                ></img>
+              </div>
+            ))}
+          </div>
+        </div>
+      </div>
       <div className="flex justify-center pt-10">
         <CarouselDemo filmes={filmes}></CarouselDemo>
       </div>
