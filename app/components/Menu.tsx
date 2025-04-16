052b41ac3fb9af2cb3adef558b6470850711a790
LauraVert23 <laura.ribeiro@vert-capital.com>
2025-04-16 15:31:17 -0300

    menu

--- app/components/Menu.tsx
+++ app/components/Menu.tsx
@@ -1,14 +1,20 @@
+import { Menubar, MenubarMenu, MenubarTrigger } from "./ui/menubar";
 import Logo from "../images/Logo.png";
 import { AlignRight } from "lucide-react";
 export default function Menu() {
   return (
-    <ul>
+    <div>
       <div className="flex justify-between mt-5 ml-2 mr-2">
         <img src={Logo}></img>
+        <Menubar className="hidden md:flex">
+          <MenubarMenu>
+            <MenubarTrigger>File</MenubarTrigger>
+          </MenubarMenu>
+        </Menubar>
         <div className="w-[40px] h-[35px]  border-3 rounded-md border-border flex justify-center bg-card ">
           <AlignRight color="#a29a9a" className="mt-0.75" />
         </div>
       </div>
-    </ul>
+    </div>
   );
 }
