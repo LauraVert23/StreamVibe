a12c06a9e1f04f85d08aa3eeb9449ece5fcd2627
LauraVert23 <laura.ribeiro@vert-capital.com>
2025-05-13 14:54:38 -0300

    correcao

--- vite.config.ts
+++ vite.config.ts
@@ -13,11 +13,6 @@ export default defineConfig({
       configureServer(server) {
         server.middlewares.use((req, res, next) => {
           res.setHeader("Accept-CH", "Viewport-Width");
-          if (req.url?.startsWith("/.well-known")) {
-            res.statusCode = 404; 
-            res.end();
-            return;
-          }
           next();
         });
       },
