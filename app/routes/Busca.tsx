a901edd6beec1c7554876538637b2383d7d5616c
LauraVert23 <laura.ribeiro@vert-capital.com>
2025-05-14 14:53:31 -0300

    teste_automacao

--- app/routes/Busca.tsx
+++ app/routes/Busca.tsx
@@ -62,7 +62,7 @@ export default function Busca() {
         />
         {!query && (
           <h1 className="text-chart-5 lg:text-xl xl:text-2xl">
-            Faça uma busca por favor
+            Faça uma busca
           </h1>
         )}
         <React.Suspense
